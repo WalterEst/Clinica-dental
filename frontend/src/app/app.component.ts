f527146f467bd962f4a1933786fef13ccd8f8a28
benjabaza1 <bazabenja@gmail.com>
2025-06-02 09:44:19 -0400

    barrayfooter

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './estatico/nav-bar/nav-bar.component';
+import { FooterComponent } from "./estatico/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/estatico/nav-bar/nav-bar.component.ts
+++ frontend/src/app/estatico/nav-bar/nav-bar.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common'; // Esta es la función clave
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  standalone: true,
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements AfterViewInit {
 
+  // Inyectamos el ID de la plataforma actual
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  // Esto se ejecuta después de que la vista del componente se ha inicializado
+  ngAfterViewInit(): void {
+    // Solo ejecutamos esta lógica si estamos en el navegador (no en SSR)
+    if (isPlatformBrowser(this.platformId)) {
+      const button = document.getElementById('mobile-menu-button');
+      const menu = document.getElementById('mobile-menu');
+
+      button?.addEventListener('click', () => {
+        menu?.classList.toggle('show');
+      });
+    }
+  }
 }
