ea4e8632fc5767a8b46af33b701c63c7c93a4b45
WalterEVB <147894080+WalterEst@users.noreply.github.com>
2025-06-24 11:22:53 -0400

    feat: fetch patient list and detail

--- frontend/src/app/pacientes/lista-paciente/lista-paciente.component.ts
+++ frontend/src/app/pacientes/lista-paciente/lista-paciente.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NavBarComponent } from "../../estatico/nav-bar/nav-bar.component";
 import { FooterComponent } from "../../estatico/footer/footer.component";
+import { PacienteService, Paciente } from '../../servicios/paciente.service';
 
 @Component({
   selector: 'app-lista-paciente',
   standalone: true,
-  imports: [RouterModule, NavBarComponent, FooterComponent],
+  imports: [CommonModule, RouterModule, NavBarComponent, FooterComponent],
   templateUrl: './lista-paciente.component.html',
   styleUrls: ['./lista-paciente.component.css']
 })
-export class ListaPacienteComponent {
+export class ListaPacienteComponent implements OnInit {
+  pacientes: Paciente[] = [];
 
+  constructor(private pacienteService: PacienteService) {}
+
+  ngOnInit(): void {
+    this.cargarPacientes();
+  }
+
+  cargarPacientes(): void {
+    this.pacienteService.getAll().subscribe({
+      next: data => (this.pacientes = data),
+      error: err => console.error('Error al cargar pacientes', err)
+    });
+  }
 }
